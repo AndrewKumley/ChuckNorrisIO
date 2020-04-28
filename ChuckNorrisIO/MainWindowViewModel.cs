ad1a2ba3d6c7ab4548936fae3f243c4f0dde0f42
Andrew Kumley <Andrew.Kumley@stmartin.edu>
2020-04-28 10:47:39 -0700

    -

---
+++ ChuckNorrisIO/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace ChuckNorrisIO
+{
+    class MainWindowViewModel
+    {
+        private string _joke;
+        private Services.ChuckNorrisJokeClient Client { get; }
+        private IEnumerable<string> _categories;
+
+        public MainWindowViewModel()
+        {
+            Client = new Services.ChuckNorrisJokeClient();
+            GetRandomJoke();
+            _categories = Client.Categories();
+        }
+
+        private void GetRandomJoke()
+        {
+            Services.ChuckNorrisJokeClient jokeClient = new Services.ChuckNorrisJokeClient();
+            IEnumerable<string> jokes = jokeClient.Random();
+            _joke = jokes.First();
+        }
+
+        public IEnumerable<string> Categories
+        {
+            get { return _categories; }
+        }
+        public string CurrentJoke
+        {
+            get { return _joke; }
+        }
+
+        private void SetRandomJoke()
+        {
+
+        }
+    }
+
+
+}
---
+++ ChuckNorrisIO/Services/ChuckNorrisJokeClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace ChuckNorrisIO.Services
+{
+    public class ChuckNorrisJokeClient
+    {
+        public IEnumerable<string> Categories()
+        {
+            System.Net.Http.HttpClient httpClient = new HttpClient();
+            System.Net.Http.HttpResponseMessage response = httpClient.GetAsync("https://api.chucknorris.io/jokes/categories").Result;
+            string content = response.Content.ReadAsStringAsync().Result;
+
+            string[] result = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]>(content);
+
+            response.Dispose();
+            httpClient.Dispose();
+
+            return result;
+        }
+
+        public IEnumerable<string> Random()
+        {
+            System.Net.Http.HttpClient httpClient = new HttpClient();
+            System.Net.Http.HttpResponseMessage response = httpClient.GetAsync("https://api.chucknorris.io/jokes/categories").Result;
+            string content = response.Content.ReadAsStringAsync().Result;
+
+            Joke result = Newtonsoft.Json.JsonConvert.DeserializeObject<Joke>(content);
+
+            response.Dispose();
+            httpClient.Dispose();
+
+            return new string[] { result.Value };
+        }
+
+        private string GetContent(string uri)
+        {
+            System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+            System.Net.Http.HttpResponseMessage response = httpClient.GetAsync(uri).Result;
+            string content = response.Content.ReadAsStringAsync().Result;
+            response.Dispose();
+            httpClient.Dispose();
+
+            return content;
+        }
+
+        private class Joke
+        {
+            public string[] Categories { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
